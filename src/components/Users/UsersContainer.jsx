e68d382704c8bf34de554a0dbfee86b09f39c5ce
DarkSharkk <telio2016@yandex.ru>
2022-11-20 17:55:38 +0300

    moved api logic to container component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,5 +1,5 @@
+import React from "react";
 import { connect } from "react-redux";
-import Users from "./Users";
 import {
     followActionCreator,
     unfollowActionCreator,
@@ -7,6 +7,40 @@ import {
     setCurrentPageActionCreator,
     setTotalUsersCountActionCreator
 } from "../../redux/usersReducer";
+import axios from "axios";
+import Users from "./Users";
+
+class UsersAPI extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount)
+            });
+    }
+
+    onPageChange = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            })
+    }
+
+    render() {
+        return (
+            <Users 
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChange={this.onPageChange}
+                users={this.props.users}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow} 
+            />
+        )
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -37,6 +71,6 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPI);
 
 export default UsersContainer;
