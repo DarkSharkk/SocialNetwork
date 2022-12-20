dff6cf307f052d16e37b6fc990eed97b7a9a49c1
DarkSharkk <telio2016@yandex.ru>
2022-12-20 20:21:02 +0300

    add follow/unfollow logic with API communication

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Users.module.css";
 import userPhoto from '../../assets/images/index.png';
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 const Users = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -30,8 +31,34 @@ const Users = (props) => {
                         </div>
                         <div>
                             {u.followed ?
-                                <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button> :
-                                <button onClick={() => { props.follow(u.id) }}>Follow</button>
+                                <button 
+                                    onClick={() => { 
+
+                                            axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                                withCredentials: true
+                                            }).then((response) => {
+                                                if (response.data.resultCode === 0) {
+                                                    props.unfollow(u.id)
+                                                } 
+                                            })
+                                        }
+                                    }
+                                >Unfollow</button> 
+                                :
+                                <button 
+                                    onClick={() => { 
+
+                                            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                                withCredentials: true
+                                            }).then((response) => {
+                                                if (response.data.resultCode === 0) {
+                                                    props.follow(u.id) 
+                                                } 
+                                            })                                        
+                                            
+                                        }
+                                    }
+                                >Follow</button>
                             }
                         </div>
                     </span>
